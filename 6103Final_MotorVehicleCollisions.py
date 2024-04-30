9f6341d5425e4ca2799353e251b47b7561cae029
Alice Pascalev <112493328+a-pas@users.noreply.github.com>
2024-04-30 19:10:48 -0400

    Uploading Project Files

---
+++ 6103Final_MotorVehicleCollisions.py
@@ -0,0 +1,10 @@
+
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+
+collisions = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes_v11.18.22.csv')
+#%%
